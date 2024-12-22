dff01b8971ddf69699875d0c59b8bf60bebfb034
Kunal Paraye <kunalparaye39@gmail.com>
2024-12-23 02:43:53 +0530

    Model Setup

---
+++ models/product-model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+    image : String,
+    name : String,
+    price : Number,
+    disscount : {
+        type : Number,
+        default : 0,
+    },
+    bgcolor : String,
+    panelcolor : String,
+    textcolor : String,
+});
+
+
+module.exports = mongoose.Model('product', productSchema);
