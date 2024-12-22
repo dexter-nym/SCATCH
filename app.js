726531f86377e22ef609219ae423c5ab00d25ac5
Kunal Paraye <kunalparaye39@gmail.com>
2024-12-23 03:39:15 +0530

    Mongoose connection fix

--- app.js
+++ app.js
@@ -7,19 +7,16 @@ const db = require('./config/mongoose-connection')
 const cookieParser = require('cookie-parser');
 const path = require('path');
 const ownersRouter = require('./routes/ownersRouter');
-const ownersRouter = require('./routes/ownersRouter');
 const productsRouter = require('./routes/productsRouter');
 const usersRouter = require('./routes/usersRouter');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
-app.use(express.static(__dirname, 'public'))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/owners', ownersRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
 
-app.listen(port, function (req, res) {
-    console.log('Server started');
-});
+app.listen(port);
