24c8d094738a1f43026ba9114180cab220fc013c
Kunal Paraye <kunalparaye39@gmail.com>
2024-12-23 03:32:00 +0530

    Routes Setup

--- app.js
+++ app.js
@@ -6,23 +6,19 @@ const port = 3000;
 const db = require('./config/mongoose-connection')
 const cookieParser = require('cookie-parser');
 const path = require('path');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const multer = require('multer');
+const ownersRouter = require('./routes/ownersRouter');
+const ownersRouter = require('./routes/ownersRouter');
+const productsRouter = require('./routes/productsRouter');
+const usersRouter = require('./routes/usersRouter');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(__dirname, 'public'))
-app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-    try{
-        res.send('Welcome, Current route "/"');
-    } catch(error){
-        console.log(error);       
-    }
-});
+app.use('/owners', ownersRouter);
+app.use('/users', usersRouter);
+app.use('/products', productsRouter);
 
 app.listen(port, function (req, res) {
     console.log('Server started');
