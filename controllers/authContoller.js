8086281309a4d638f1dbf728edd82d9ee50907b4
Kunal Paraye <kunalparaye39@gmail.com>
2024-12-23 18:04:19 +0530

    Login route setup stage 1

--- controllers/authContoller.js
+++ controllers/authContoller.js
@@ -1,6 +1,6 @@
 const userModel = require('../models/user-model');
 const bcrypt = require('bcrypt');
-const cookieParser = ('cookie-parser');
+const cookieParser = require('cookie-parser');
 const { generateToken } = require('../utils/generateTokens');
 
 module.exports.registerUser = async function(req, res) {
@@ -24,10 +24,33 @@ module.exports.registerUser = async function(req, res) {
                     });
                     let token = generateToken(user);
                     res.cookie('token', token);
-                    res.send(`${req.cookies['token']}, user created`)
+                    res.send(`User created, token: ${token}`);
                 };
             });
         });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+module.exports.loginUser = async function(req, res) {
+    try{
+        let {email, password} = req.body;
+        let user = await userModel.findOne({email: email});
+        if(!user){
+            return res.send("Email or password incorrect");
+        }
+
+        bcrypt.compare(password, user.password, function (err, result) {
+            if(result){
+                let token = generateToken(user);
+                res.cookie('token', token);
+                res.send('Login successfult')
+            } else {
+                return res.send("Email or password incorrect");
+            }
+        });
+        
     } catch (error) {
         console.log(error.message);
     }
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser } = require('../controllers/authContoller')
+const { registerUser, loginUser } = require('../controllers/authContoller')
 
 router.get('/', function (req, res) {
     res.send('Users Route');
 });
 
 router.post('/register', registerUser);
+
+router.post('/login', loginUser);
 
 module.exports = router;
